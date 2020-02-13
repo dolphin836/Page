dae35ac47dee154c1b55e0f6b0ca691e2496b865
dolphin <416509859@qq.com>
2020-02-13 18:32:37 +0800

    Build

---
+++ build/precache-manifest.c0b18e933aab78b12e909d59e06b6bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3536b9207601ac02194ae201f4183b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b76a37141bd379b444",
+    "url": "/static/css/2.aa100c54.chunk.css"
+  },
+  {
+    "revision": "a0da03749adca50cca39",
+    "url": "/static/css/main.c737e34e.chunk.css"
+  },
+  {
+    "revision": "21b76a37141bd379b444",
+    "url": "/static/js/2.6679bca5.chunk.js"
+  },
+  {
+    "revision": "a0da03749adca50cca39",
+    "url": "/static/js/main.5e8424bd.chunk.js"
+  },
+  {
+    "revision": "0d285dd0c84d4bcc960b",
+    "url": "/static/js/runtime~main.ecbc3359.js"
+  }
+]);
