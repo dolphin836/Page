1bf86351b50a858c78d9ba615ea16cf3eeca1df7
wanghaibing <wanghaibing836@gmail.com>
2020-09-03 18:59:51 +0800

    some

--- src/Page/Video.js
+++ src/Page/Video.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import Layout from '../Component/Layout';
 import { Player } from 'video-react';
+import { Title } from "rbx";
 import "../../node_modules/video-react/dist/video-react.css"; // import css
 
 class Video extends PureComponent {
@@ -8,7 +9,9 @@ class Video extends PureComponent {
         super(props);
 
         this.state = {
-            data: []
+             name: '',
+            video: '',
+             list: []
         };
     }
 
@@ -21,16 +24,27 @@ class Video extends PureComponent {
                     let json = response.json();
                     
                     json.then(function(source) {
-                        let data;
+                        let name;
+                        let video;
+                        let list;
 
                         source.map((channel) => {
                             if (channel.code === _self.props.match.params.Code) {
-                                data = channel;
+                                name = channel.name;
+
+                                if (channel.list) {
+                                    video = channel.list[0].video;
+                                    list  = channel.list;
+                                } else {
+                                    video = channel.video;
+                                }
                             }
                         });
                         // 更新数据
                         _self.setState({
-                            data: data
+                             name: name,
+                            video: video,
+                             list: list
                         });
                     });
                 }
@@ -38,13 +52,24 @@ class Video extends PureComponent {
             .catch(function (error) {
                 console.log(JSON.stringify(error));
             });
-    
+    }
+
+    switchVideo = (channel) => {
+        this.setState({
+            video: channel.video
+        });
     }
 
     render() {
         return (
-            <Layout title={ this.state.data.name } back="/Video" backname="Video">
-                <Player autoPlay={ true } src={ this.state.data.video } />
+            <Layout title={ this.state.name } back="/Video" backname="Video">
+                <Player autoPlay={ true } src={ this.state.video } />
+
+                { this.state.list.map((channel, i) =>
+                    <Title onClick={() => this.switchVideo(channel) } key={ i } as="p" size={ 5 } subtitle className="has-text-dark has-text-weight">
+                        { channel.name }
+                    </Title>
+                )}
             </Layout>
         );
     }
